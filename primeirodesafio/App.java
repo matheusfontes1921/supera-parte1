95a00c23a88e19fd2c6731fc2ba2a42e40c67556
Matheus Fontes <matheusfontes1921@gmail.com>
2023-07-11 13:51:18 -0300

    first challenge done

--- primeirodesafio/App.java
+++ primeirodesafio/App.java
@@ -10,17 +10,26 @@ public class App {
          * Sendo que deverão ser apresentados os pares em ordem crescente e depois os ímpares
          * em ordem decrescente.
          */
+
+        /* inicialização do scanner e leitura da quantidade de números */
         Scanner sc = new Scanner(System.in);
         System.out.println("Quantos números serão fornecidos? ");
         int quantity = sc.nextInt();
+        /*inicialização da lista de números e criação da variavel numero
+         * para armazenamento de valor atual
+         */
         List<Integer> geral = new ArrayList<>();
         int numero = 0;
+        /* for com número de interações fornecido pelo usuário */
         for (int i = 1; i <= quantity; i++) {
             System.out.println("Escreva o próximo número: ");
             numero = sc.nextInt();
+            /* sempre adiciona o número fornecido pelo usuário na lista de inteiros */
             geral.add(numero);
         }
+        /* imprime os pares em ordem crescente */
         geral.stream().filter(x -> x % 2 == 0).sorted().forEach(System.out::println);
+        /* imprime os ímpares em ordem decrescente */
         geral.stream().filter(x -> x % 2 != 0).sorted(Comparator.reverseOrder()).forEach(System.out::println);
     }
 }
